4a2954b103c3233f4847a57911cc9bb2d66956c2
rebecca_wl2 <995092781wl@gmail.com>
2021-04-29 09:57:37 +0900

    no message

--- springboot-doma/src/main/java/com/springboot/doma/SpringbootDomaApplication.java
+++ springboot-doma/src/main/java/com/springboot/doma/SpringbootDomaApplication.java
@@ -1,5 +1,15 @@
 package com.springboot.doma;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +17,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDomaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDomaApplication.class, args);
+		new CopyOnWriteArrayList<>();
+		new Vector<>();
+		List<String> list=Collections.synchronizedList(new ArrayList<String>());
+		Collections.synchronizedMap(null);
+		new ConcurrentHashMap();
+		new ReentrantLock();
+		new ReentrantReadWriteLock();
+		new CyclicBarrier();
+		new Semaphore();
 	}
 }
